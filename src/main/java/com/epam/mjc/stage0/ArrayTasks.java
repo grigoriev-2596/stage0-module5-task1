f4ef10523a394f90bf9a66b951219b1898fdb9ad
grigoriev-2596 <grigoriev.egor2596@gmail.com>
2022-10-31 21:28:28 +0300

    all tasks are done

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -1,5 +1,6 @@
 package com.epam.mjc.stage0;
 
+
 /**
  * Here are the tasks for working with the arrays.
  * <p>
@@ -11,7 +12,7 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-
+        return new String[]{"Winter", "Spring", "Summer", "Autumn"};
     }
 
     /**
@@ -25,7 +26,11 @@ public class ArrayTasks {
      * length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-
+        int[] generatedArr = new int[length];
+        for (int i = 1; i <= length; i++) {
+            generatedArr[i - 1] = i;
+        }
+        return generatedArr;
     }
 
     /**
@@ -37,7 +42,11 @@ public class ArrayTasks {
      * arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
     }
 
     /**
@@ -50,7 +59,10 @@ public class ArrayTasks {
      * arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == number) return i;
+        }
+        return -1;
     }
 
     /**
@@ -63,7 +75,11 @@ public class ArrayTasks {
      * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-
+        String[] reversedArr = new String[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            reversedArr[arr.length - i - 1] = arr[i];
+        }
+        return reversedArr;
     }
 
     /**
@@ -78,7 +94,19 @@ public class ArrayTasks {
      * arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-
+        int positiveCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) positiveCount++;
+        }
+        int[] positiveArr = new int[positiveCount];
+        int positiveIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                positiveArr[positiveIndex] = arr[i];
+                positiveIndex++;
+            }
+        }
+        return positiveArr;
     }
 
     /**
@@ -91,7 +119,74 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]]
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
-    public int[][] sortRaggedArray(int[][] arr) {
-
+    public static int[][] sortRaggedArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            quickSort(arr[i], 0, arr[i].length - 1);
+        }
+        quickSort(arr, 0, arr.length - 1);
+        return arr;
     }
+
+    private static void quickSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int pivot = partition(arr, start, end);
+
+            quickSort(arr, start, pivot - 1);
+            quickSort(arr, pivot + 1, end);
+        }
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+        int wall = start - 1;
+        for (int i = start; i < end; i++) {
+            if (arr[i] < pivot) {
+                wall++;
+                swap(arr, i, wall);
+            }
+        }
+        swap(arr, end, wall + 1);
+        return (wall + 1);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static void quickSort(int[][] arr, int start, int end) {
+        if (start < end) {
+            int pivot = partition(arr, start, end);
+
+            quickSort(arr, start, pivot - 1);
+            quickSort(arr, pivot + 1, end);
+        }
+    }
+
+    private static int partition(int[][] arr, int start, int end) {
+        int[] pivot = arr[end];
+        int wall = start - 1;
+        for (int i = start; i < end; i++) {
+            if (arr[i].length < pivot.length) {
+                wall++;
+                swap(arr, i, wall);
+            }
+        }
+        swap(arr, end, wall + 1);
+        return (wall + 1);
+    }
+
+    private static void swap(int[][] arr, int i, int j) {
+        int[] temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[][] arrTest = new int[][]{new int[]{2, 1}, new int[]{3}, new int[]{1, 6, 3}, new int[] {5, 6}};
+//        sortRaggedArray(arrTest);
+//        System.out.println(Arrays.deepToString(arrTest));
+    }
+
 }
